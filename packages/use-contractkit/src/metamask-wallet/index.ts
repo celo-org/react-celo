c14c9ab868dd0e7fdd7b05cd7a5994dc02476a45
Alex Harley <alexbruceharley@gmail.com>
2021-02-28 10:42:44 +0100

    chore: fix metamask wallet build

--- packages/use-contractkit/src/metamask-wallet/index.ts
+++ packages/use-contractkit/src/metamask-wallet/index.ts
@@ -1,10 +1,8 @@
-import { CeloTx, Signer } from '@celo/connect';
-import { ContractKit } from '@celo/contractkit';
+import { Signer } from '@celo/connect';
 import { EIP712TypedData } from '@celo/utils/lib/sign-typed-data-utils';
 import { RemoteWallet } from '@celo/wallet-remote';
-import Web3 from 'web3';
 import { fromRpcSig } from 'ethereumjs-util';
-import { sign } from 'crypto';
+import Web3 from 'web3';
 
 export class MetamaskSigner implements Signer {
   constructor(protected address: string, protected web3: Web3) {}
@@ -13,39 +11,18 @@ export class MetamaskSigner implements Signer {
     throw new Error('signTransaction unimplemented; use signRawTransaction');
   }
 
-  // async signRawTransaction(tx: CeloTx) {
-  //   console.log('a1');
-  //   const a = await this.web3.eth.signTransaction(tx);
-  //   console.log('a2', a);
-  //   const signedTx = await new Promise((resolve, reject) => {
-  //     this.web3.currentProvider!.sendAsync(
-  //       {
-  //         method: 'eth_signTransaction',
-  //         params: [tx],
-  //       },
-  //       (err, result) => {
-  //         if (err) reject(err);
-  //         else if (result.error) reject(err);
-  //         else resolve(result);
-  //       }
-  //     );
-  //   });
-  //   console.log('>>>', signedTx);
-
-  //   return null;
-  // }
-
   async signTypedData(
     typedData: EIP712TypedData
   ): Promise<{ v: number; r: Buffer; s: Buffer }> {
-    console.log('>>>', typedData);
     const { result } = await new Promise((resolve, reject) => {
+      // @ts-ignore
       this.web3.currentProvider!.sendAsync(
         {
           method: 'eth_signTypedData_v4',
           params: [this.address, JSON.stringify(typedData)],
           from: this.address,
         },
+        // @ts-ignore
         (err, result) => {
           if (err) reject(err);
           else if (result.error) reject(err);
@@ -60,8 +37,9 @@ export class MetamaskSigner implements Signer {
   async signPersonalMessage(
     data: string
   ): Promise<{ v: number; r: Buffer; s: Buffer }> {
-    const a = await this.web3.eth.personal.sign(data, this.address);
-    return fromRpcSig(a) as { v: number; r: Buffer; s: Buffer };
+    // @ts-ignore
+    const result = await this.web3.eth.personal.sign(data, this.address);
+    return fromRpcSig(result) as { v: number; r: Buffer; s: Buffer };
   }
 
   getNativeKey = () => this.address;
@@ -91,16 +69,4 @@ export class MetamaskWallet extends RemoteWallet<MetamaskSigner> {
     });
     return addressToSigner;
   }
-
-  // /**
-  //  * Gets the signer based on the 'from' field in the tx body
-  //  * @param txParams Transaction to sign
-  //  * @dev overrides WalletBase.signTransaction
-  //  */
-  // async signTransaction(txParams: CeloTx) {
-  //   console.log('signing', txParams);
-  //   const address = txParams.from!;
-  //   const signer = this.getSigner(address as string);
-  //   return signer.signRawTransaction(txParams);
-  // }
 }
