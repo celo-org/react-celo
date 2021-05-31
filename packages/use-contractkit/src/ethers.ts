f61f6e0f10ab3ba849f51f5032f9dfa251f2e349
Ian Macalinao <ian@ubeswap.org>
2021-05-31 01:03:05 -0500

    Add ethers helpers

---
+++ packages/use-contractkit/src/ethers.ts
@@ -0,0 +1,67 @@
+import { useContractKit } from './use-contractkit';
+import { JsonRpcSigner, Web3Provider } from '@ethersproject/providers';
+import { ExternalProvider } from '@ethersproject/providers/lib/web3-provider';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+
+export const useProvider = (): Web3Provider => {
+  const { kit } = useContractKit();
+  const provider = (kit.web3.currentProvider as unknown) as ExternalProvider;
+  return useMemo(() => {
+    return new Web3Provider(provider);
+  }, [provider]);
+};
+
+export const useProviderOrSigner = (): Web3Provider | JsonRpcSigner => {
+  const { kit } = useContractKit();
+  const provider = useProvider();
+  return useMemo(() => {
+    return kit.defaultAccount
+      ? provider.getSigner(kit.defaultAccount)
+      : provider;
+  }, [provider, kit.defaultAccount]);
+};
+
+export const useGetConnectedSigner = (): (() => Promise<JsonRpcSigner>) => {
+  const { kit, connect } = useContractKit();
+  const signer = useProviderOrSigner();
+  return useCallback(async () => {
+    if (kit.defaultAccount) {
+      return signer as JsonRpcSigner;
+    }
+    const connector = await connect();
+    const nextKit = await connector.initialise();
+    const nextProvider = (nextKit.kit.web3
+      .currentProvider as unknown) as ExternalProvider;
+    return new Web3Provider(nextProvider).getSigner(nextKit.kit.defaultAccount);
+  }, [signer, kit, connect]);
+};
+
+export const useLazyConnectedSigner = (): {
+  signer: JsonRpcSigner | null;
+  address: string | null;
+  getConnectedSigner: () => Promise<JsonRpcSigner>;
+} => {
+  const getConnectedSigner = useGetConnectedSigner();
+  const [signer, setSigner] = useState<JsonRpcSigner | null>(null);
+  const getConnectedSignerCb = useCallback(async () => {
+    const theSigner = await getConnectedSigner();
+    setSigner(theSigner);
+    return theSigner;
+  }, [getConnectedSigner, setSigner]);
+  return {
+    signer,
+    getConnectedSigner: getConnectedSignerCb,
+    address: signer?._address ?? null,
+  };
+};
+
+export const useConnectedSigner = (): JsonRpcSigner | null => {
+  const getConnectedSigner = useGetConnectedSigner();
+  const [signer, setSigner] = useState<JsonRpcSigner | null>(null);
+  useEffect(() => {
+    void (async () => {
+      setSigner(await getConnectedSigner());
+    })();
+  }, [getConnectedSigner, setSigner]);
+  return signer;
+};
--- packages/use-contractkit/src/index.ts
+++ packages/use-contractkit/src/index.ts
@@ -1,5 +1,6 @@
 export * from './constants';
 export * from './use-contractkit';
 export * from './types';
+export * from './ethers';
 
 export { defaultScreens as Screens } from './screens';
