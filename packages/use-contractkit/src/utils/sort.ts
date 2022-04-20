06f86d2b6ea02b6e9f4500cc7236e693d78840fa
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2022-04-20 15:11:08 +0200

    feat: add modal customization options (#153)

---
+++ packages/use-contractkit/src/utils/sort.ts
@@ -0,0 +1,25 @@
+import { Provider } from '../types';
+
+export type SortingPredicate<T> = (a: T, b: T) => -1 | 0 | 1;
+
+export const sortString: SortingPredicate<string> = (a, b) => {
+  const A = a.toUpperCase();
+  const B = b.toUpperCase();
+
+  if (A < B) return -1;
+  if (A > B) return 1;
+  return 0;
+};
+
+export const sortByPriority: SortingPredicate<Provider> = (a, b) => {
+  const A = a.listPriority();
+  const B = b.listPriority();
+
+  if (A < B) return -1;
+  if (A > B) return 1;
+  return 0;
+};
+
+export const defaultProviderSort: SortingPredicate<Provider> = (a, b) => {
+  return sortByPriority(a, b) || sortString(a.name, b.name);
+};
