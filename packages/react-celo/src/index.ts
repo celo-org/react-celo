3a8c829c3a1fad276e641cfd12b3b19e1c501b15
Camila Rondinini <crondinini@gmail.com>
2022-05-23 18:07:25 -0400

    fix: export useContractKit

--- packages/react-celo/src/index.ts
+++ packages/react-celo/src/index.ts
@@ -3,4 +3,4 @@ export * from './ethers';
 export * from './react-celo-provider';
 export { defaultScreens as Screens } from './screens';
 export * from './types';
-export { UseCelo, useCelo } from './use-celo';
+export { UseCelo, useCelo, useContractKit } from './use-celo';
