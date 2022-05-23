3072e631d797a06dbfec67b10e1953ab16deff3c
Camila Rondinini <crondinini@gmail.com>
2022-05-23 11:36:24 -0400

    feat: add new items to test plan page

--- packages/example/pages/wallet-test-plan.tsx
+++ packages/example/pages/wallet-test-plan.tsx
@@ -1,10 +1,13 @@
-import { CeloProvider, Mainnet } from '@celo/react-celo';
+import { CeloProvider, Mainnet, useCelo } from '@celo/react-celo';
 import React from 'react';
 
 import { ConnectWalletCheck } from '../components/test-plan/connect-wallet';
+import { SendTransaction } from '../components/test-plan/send-transaction';
 import { SwitchNetwork } from '../components/test-plan/switch-networks';
+import { UpdateFeeCurrency } from '../components/test-plan/update-fee-currency';
 
 export default function WalletTestPlan(): React.ReactElement {
+  const { destroy } = useCelo();
   return (
     <CeloProvider
       dapp={{
@@ -23,9 +26,22 @@ export default function WalletTestPlan(): React.ReactElement {
         <div className="text-slate-600 mt-2">
           A set of steps to help verify how well a given wallet interacts with
           react-celo.
+          <br />
+          You will need to connect a wallet before being able to run any other
+          test.
+        </div>
+        <div>
+          <button
+            className="inline underline text-purple-700"
+            onClick={destroy}
+          >
+            Disconnect wallet
+          </button>
         </div>
         <ConnectWalletCheck />
         <SwitchNetwork />
+        <UpdateFeeCurrency />
+        <SendTransaction />
       </div>
     </CeloProvider>
   );
