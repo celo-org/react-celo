a955cf88040b39f2ff58351b07a5fa70af4e9a4f
Camila Rondinini <crondinini@gmail.com>
2022-05-23 11:35:30 -0400

    feat: add test for updating fee currency

---
+++ packages/example/components/test-plan/update-fee-currency.tsx
@@ -0,0 +1,52 @@
+import { CeloContract } from '@celo/contractkit';
+import { useCelo } from '@celo/react-celo';
+import { useEffect } from 'react';
+
+import { SuccessIcon } from './success-icon';
+import { Result, TestBlock } from './ui';
+import { useDisabledTest } from './useDisabledTest';
+import { useTestStatus } from './useTestStatus';
+
+export function UpdateFeeCurrency() {
+  const { updateFeeCurrency, feeCurrency, supportsFeeCurrency } = useCelo();
+  const [disabledTest, setDisabledTest] = useDisabledTest();
+  const { status, errorMessage, wrapActionWithStatus, setStatus } =
+    useTestStatus();
+
+  useEffect(() => {
+    if (supportsFeeCurrency !== undefined && !supportsFeeCurrency) {
+      setDisabledTest(true);
+      setStatus.error('Wallet does not support updating fee currency.');
+    }
+  }, [supportsFeeCurrency, setStatus, setDisabledTest]);
+
+  const onUpdateCurrency = wrapActionWithStatus(async () => {
+    setDisabledTest(true);
+    await updateFeeCurrency(CeloContract.StableTokenBRL);
+    if (feeCurrency !== CeloContract.StableTokenBRL) {
+      throw new Error('Fee currency did not update.');
+    }
+  });
+
+  return (
+    <TestBlock
+      status={status}
+      title="Update fee currency"
+      disabledTest={disabledTest}
+      onRunTest={onUpdateCurrency}
+    >
+      <Result status={status}>
+        <p>Fee currency used: {feeCurrency}</p>
+        <Result.Default>
+          <>
+            <p>Change the currency used in transactions.</p>
+          </>
+        </Result.Default>
+        <Result.Success>
+          <SuccessIcon /> Fee currency {feeCurrency}
+        </Result.Success>
+        <Result.Error>{errorMessage}</Result.Error>
+      </Result>
+    </TestBlock>
+  );
+}
