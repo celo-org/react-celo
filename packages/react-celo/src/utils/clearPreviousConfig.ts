f0001beb00a7eea433b66f2aa2e0c5b015f8afde
Aaron <aaron@clabs.co>
2022-05-31 16:36:13 -0700

    remove extra file

--- packages/react-celo/src/utils/clearPreviousConfig.ts
+++
@@ -1,9 +0,0 @@
-import { localStorageKeys } from '../constants';
-
-export function clearPreviousConfig(): void {
-  Object.values(localStorageKeys).forEach((val) => {
-    if (val === localStorageKeys.lastUsedWalletId) return;
-    if (val === localStorageKeys.lastUsedWalletType) return;
-    localStorage.removeItem(val);
-  });
-}
