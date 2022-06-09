259b6865e9c1825fc7eea068dc31c559fd1618a6
Aaron <aaron@clabs.co>
2022-06-09 11:18:11 -0700

    add private key test (same private key used in example wallet)
    add cypress test commands

--- packages/example/cypress/e2e/wallet-test-plan.cy.ts
+++ packages/example/cypress/e2e/wallet-test-plan.cy.ts
@@ -27,19 +27,18 @@ context('Wallet Test Plan', () => {
     cy.get('input[role="search"]').clear();
   });
 
-  it('Connects with wallet connect', () => {
+  it('Connects with Private Key', () => {
     openModal();
-    cy.findByText('Celo Wallet').click();
+    cy.findByText('Private key').click();
 
-    // cy.get('button[aria-label="Connect with Celo Wallet Web"]').click();
+    cy.get('textarea').type(
+      'e2d7138baa3a5600ac37984e40981591d7cf857bcadd7dc6f7d14023a17b0787'
+    );
+    cy.findByText('Submit').click();
 
-    cy.origin('celowallet.app', () => {
-      cy.visit('https://celowallet.app');
-      cy.get('button')
-        .should('contain.text', 'Create New Account')
-        .first()
-        .click();
-    });
+    cy.findByText('Connected to').should('exist');
+
+    cy.findAllByText('success').should('have.length', 1);
   });
 });
 
