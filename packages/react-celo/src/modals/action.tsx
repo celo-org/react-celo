5c2ff0d7a99470f26475a0bc2b6b3f33393b6eee
Aaron DeRuvo <aaron@clabs.co>
2022-06-17 07:48:24 -0700

    Fixes #268 (-webkit-fill-available vs background gradient bug)
    use react modals custom classname to add the styles to html and body only when our modal is open

--- packages/react-celo/src/modals/action.tsx
+++ packages/react-celo/src/modals/action.tsx
@@ -94,6 +94,8 @@ export const ActionModal: React.FC<Props> = ({
   return (
     <ReactModal
       portalClassName={styles.portal}
+      htmlOpenClassName={'react-celo-modal-open-html'}
+      bodyOpenClassName={'react-celo-modal-open-body'}
       isOpen={pendingActionCount > 0}
       // isOpen
       ariaHideApp={false}
