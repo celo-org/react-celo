be2964c9f3a191b424d68b5390ff91b1bad9fe95
Aaron DeRuvo <aaron@clabs.co>
2022-09-07 16:33:32 +0100

    Feat #294 show up to 3 most recent wallets used (#301)
    * Fix #294 show up to 3 most recent wallets used

--- packages/react-celo/__tests__/utils/local-storage.test.ts
+++ packages/react-celo/__tests__/utils/local-storage.test.ts
@@ -1,12 +1,18 @@
 import { CeloContract } from '@celo/contractkit';
 
-import { localStorageKeys, WalletTypes } from '../../src/constants';
+import { localStorageKeys, WalletIds, WalletTypes } from '../../src/constants';
 import {
+  getRecentWallets,
   getTypedStorageKey,
+  rememberWallet,
   setTypedStorageKey,
+  wipeStorage,
 } from '../../src/utils/local-storage';
 
 describe('TypedLocalStorage', () => {
+  beforeEach(() => {
+    wipeStorage();
+  });
   describe('get/setTypedStorageKey', () => {
     describe(localStorageKeys.lastUsedAddress, () => {
       it('sets Address', () => {
@@ -37,6 +43,63 @@ describe('TypedLocalStorage', () => {
           getTypedStorageKey(localStorageKeys.lastUsedFeeCurrency)
         ).toEqual(CeloContract.StableTokenBRL);
       });
+    });
+  });
+});
+
+describe('getRecentWallets', () => {
+  beforeEach(() => {
+    wipeStorage();
+  });
+  it('returns an empty array when nothing is there', () => {
+    expect(getRecentWallets()).toEqual([]);
+  });
+  it('returns array of {type} strings when they have been added', () => {
+    setTypedStorageKey(
+      localStorageKeys.lastUsedWallets,
+      JSON.stringify([`${WalletTypes.MetaMask}`])
+    );
+    expect(getRecentWallets()).toEqual([WalletTypes.MetaMask]);
+  });
+});
+
+describe('rememberWallet', () => {
+  beforeEach(() => {
+    wipeStorage();
+  });
+  describe('when storing for first time', () => {
+    it('saves it', () => {
+      rememberWallet(WalletTypes.Valora, WalletIds.Valora);
+      expect(getRecentWallets()).toEqual([
+        `${WalletTypes.Valora}:${WalletIds.Valora}`,
+      ]);
+    });
+  });
+  describe('when storing A BA C', () => {
+    it('will be storing B A C', () => {
+      rememberWallet(WalletTypes.Valora, WalletIds.Valora);
+      rememberWallet(WalletTypes.MetaMask);
+      rememberWallet(WalletTypes.Valora, WalletIds.Valora);
+      rememberWallet(WalletTypes.CeloTerminal, WalletIds.CeloTerminal);
+      expect(getRecentWallets()).toEqual([
+        `${WalletTypes.CeloTerminal}:${WalletIds.CeloTerminal}`,
+        `${WalletTypes.Valora}:${WalletIds.Valora}`,
+        WalletTypes.MetaMask,
+      ]);
+    });
+  });
+  describe('when at max size', () => {
+    it('adds it and removes last item', () => {
+      rememberWallet(WalletTypes.Valora, WalletIds.Valora);
+      rememberWallet(WalletTypes.MetaMask);
+      rememberWallet(WalletTypes.WalletConnect, WalletIds.Omni);
+      rememberWallet(WalletTypes.CeloTerminal, WalletIds.CeloTerminal);
+
+      expect(getRecentWallets()).toEqual([
+        `${WalletTypes.CeloTerminal}:${WalletIds.CeloTerminal}`,
+        `${WalletTypes.WalletConnect}:${WalletIds.Omni}`,
+        WalletTypes.MetaMask,
+      ]);
     });
   });
 });
