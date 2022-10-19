23de0bc43fe7e88401e31222d41210f752aa9b93
Aaron <aaron@clabs.co>
2022-10-19 12:11:35 +0200

    no fallbacks

--- packages/example/next.config.js
+++ packages/example/next.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   webpack: (config, { webpack }) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      child_process: false,
-      readline: false,
-    };
     config.plugins.push(
       new webpack.IgnorePlugin({ resourceRegExp: /^electron$/ })
     );
